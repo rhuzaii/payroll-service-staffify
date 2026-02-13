e6c1f8fd80b58ac66ca2e4c2302767c257c7cc1e
ranaadiba <ranaadiba71@gmail.com>
2026-02-14 02:06:02 +0530

    Initial commit: Payroll Service running

---
+++ src/test/java/com/staffify/payroll/PayrollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staffify.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
